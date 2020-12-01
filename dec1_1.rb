371e16e93188587da79a24e8e8a9508b6473e32f
Chris Lorig <christian.lorig@toptal.com>
2020-12-01 22:56:58 +0100

    Clean up repo for 2020

--- dec1_1.rb
+++
@@ -1,9 +0,0 @@
-require_relative 'advent_data'
-
-data = AdventData.new(day: 1, session: ARGV[0]).get
-
-modules_fuel = data.map { |module_mass| module_mass.to_i / 3 - 2 }
-
-total_fuel = modules_fuel.reduce(&:+)
-
-puts total_fuel
