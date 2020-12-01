54562cb7d229b5c97b93734712a1ea502b13b3e0
Chris Lorig <christian.lorig@toptal.com>
2020-12-01 23:47:22 +0100

    Fix bug in the second challenge of day 1

--- day1_2.rb
+++ day1_2.rb
@@ -2,7 +2,8 @@ require_relative 'advent_data'
 
 data = AdventData.new(day: 1).get
 
-data.combination(3) do |a,b,c|
-  next unless a+b+c == 2020
-  puts [a,b,c].reduce(:*)
+data.map(&:to_i).combination(3) do |elements|
+  next unless elements.reduce(:+) == 2020
+  puts elements.reduce(:*)
+  break
 end
