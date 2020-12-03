3b24ea4d489b78d0e6eb855c89c5fed6d996d792
Chris Lorig <christian.lorig@toptal.com>
2020-12-04 00:39:33 +0100

    Refactor solution for part two of day three

--- day3_2.rb
+++ day3_2.rb
@@ -10,50 +10,28 @@ data = data.map do |line|
       .map(&:to_i) # turn the map into a matrix
 end
 
-data_matrix = Matrix.rows(data)
+@data_matrix = Matrix.rows(data)
 
 # Starting point of traversal matrix
-skip_row = Array.new(data_matrix.column_count){ 0 }
-step = skip_row.dup
-step[0] = 1
+@skip_row = Array.new(@data_matrix.column_count){ 0 }
+@step = @skip_row.dup
+@step[0] = 1
 
-# Building traversal matrices
-step11_matrix = (0...data_matrix.row_count).map do |index|
-  step.rotate(-index * 1)
-end
-step11_matrix = Matrix.rows(step11_matrix)
-
-step13_matrix = (0...data_matrix.row_count).map do |index|
-  step.rotate(-index * 3)
-end
-step13_matrix = Matrix.rows(step13_matrix)
-
-step15_matrix = (0...data_matrix.row_count).map do |index|
-  step.rotate(-index * 5)
-end
-step15_matrix = Matrix.rows(step15_matrix)
-
-step17_matrix = (0...data_matrix.row_count).map do |index|
-  step.rotate(-index * 7)
-end
-step17_matrix = Matrix.rows(step17_matrix)
-
-step21_matrix = (0...data_matrix.row_count).map do |index|
-  if index.odd?
-    skip_row
-  else 
-    step.rotate(-index / 2)
+def calculate_path_length(right, down)
+  # Building traversal matrix
+  step_matrix = (0...@data_matrix.row_count).map do |index|
+    if index % down == 0
+      @step.rotate(-((index * right) / down))
+    else 
+      @skip_row
+    end
   end
+  step_matrix = Matrix.rows(step_matrix)
+
+  @data_matrix.hadamard_product(step_matrix).to_a.flatten.sum
 end
-step21_matrix = Matrix.rows(step21_matrix)
 
-path_lengths = [
-  data_matrix.hadamard_product(step11_matrix).to_a.flatten.sum,
-  data_matrix.hadamard_product(step13_matrix).to_a.flatten.sum,
-  data_matrix.hadamard_product(step15_matrix).to_a.flatten.sum,
-  data_matrix.hadamard_product(step17_matrix).to_a.flatten.sum,
-  data_matrix.hadamard_product(step21_matrix).to_a.flatten.sum
-]
+paths = [[1,1], [3,1], [5,1], [7,1], [1,2]]
 
-puts path_lengths.reduce(:*)
+puts paths.map {|r,d| calculate_path_length(r,d)}.reduce(:*)
 
